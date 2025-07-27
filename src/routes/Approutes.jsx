a46c43297b3070f4223a56b6b53497d27c108600
anchal-github123 <anchalnayak2003@gmail.com>
2025-07-27 09:38:19 +0530

    Set up the project and Adding Routing

---
+++ src/routes/Approutes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import MainLayout from "../MainLayout";
+const roues=createBrowserRouter(
+    [
+        {
+            path:"/",
+            element:<MainLayout/>,
+            children:[
+                {
+                    index:"/",
+                    element:<Home/>,
+                },
+                {
+                    path:"/Contact",
+                    element:<Contact/>}
+            ]
+        
+        }
+    ]
+)
+
+
+export default function Approutes() {
+  return (
+          <RouterProvider router={roues}/>
+  )
+}
